22448325856e8a90fe8846e665febca8c80a3ce2
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 10:26:10 +0200

    Create 057-Pseufocódigo.py

---
+++ Practicas/057-Pseufocódigo.py
@@ -0,0 +1,27 @@
+'''
+    Aplicación de gestión de productos
+    (c) 2025 Jose Vicente Carratalá
+    Esta aplicación gestiona productor
+'''
+
+# En esta aplicación no aplica librerías
+
+# Definimos clases y funciones
+
+class Producto():
+    def __init__ (self):
+        self.nombre = ''
+        self.precio = 0
+        
+# Creamos las variables globales
+
+productor = []
+
+# Primero lanzamos un mensaje de bienvenida
+# Le mostramos al usuario las opciones que tiene 
+# En función de la opción que coja el usuario
+    # O bien creamos un nuevo producto
+    # O bien listamos los productos
+    # O bien actualizamos los productos
+    # O bien eliminamos los productos
+# Y volvemos a repetir
