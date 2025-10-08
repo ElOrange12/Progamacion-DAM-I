31b1f8676e77e636535cc03a621c3eb102795fe8
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 10:47:32 +0200

    Create 074-Repaso gato.py

---
+++ Practicas/074-Repaso gato.py
@@ -0,0 +1,11 @@
+class Gato()
+	def __init__(self): 	# El constructor se ejecuta si o si.
+		self.edad = 0
+		
+	def maulla(self):
+		return 'El gato está maullando'
+		
+gato1 = Gato()
+print(gato1.edad)
+
+print(gato1.maulla())
