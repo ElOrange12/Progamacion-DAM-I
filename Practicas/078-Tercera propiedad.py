860f91684afce1d50b7d455a6203fbd28e1ead92
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 10:49:39 +0200

    Create 078-Tercera propiedad.py

---
+++ Practicas/078-Tercera propiedad.py
@@ -0,0 +1,10 @@
+class Gato()
+	def __init__(self, edad, nombre, raza): 	# El constructor se ejecuta si o si.
+		self.edad = edad
+		self.nombre = nombre
+		self.raza = raza
+		
+	def maulla(self):
+		return 'El gato está maullando'
+		
+gato1 = Gato(5, 'micifu', 'mainecoon')
