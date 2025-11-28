b692549a948bba7ef1578401d210abe88128801c
Dani <danieloliveiravidal12@gmail.com>
2025-11-28 09:20:34 +0100

    28/11/2025

--- 002-Apuntes/001-Código/007-Utilización avanzada de clases/100-Juego/107-Nuevo parámetro.py
+++ 002-Apuntes/001-Código/007-Utilización avanzada de clases/100-Juego/107-Nuevo parámetro.py
@@ -3,13 +3,14 @@ import json
 from flask import Flask,render_template
 
 class Npc():
-    def __init__(self, x, y):
+    def __init__(self, x, y,radio):
         self.posx = x
         self.posy = y
+        self.radio = radio
 
     # Método para convertir el objeto en diccionario
     def to_dict(self):
-        return {"posx": self.posx, "posy": self.posy}
+        return {"posx": self.posx, "posy": self.posy,"radio":self.radio}
 # Preparo los personajes
 
 personajes = []
@@ -18,7 +19,8 @@ numero_personajes = 50
 for i in range(0, numero_personajes):
     xaleatoria = random.randint(0, 500)
     yaleatoria = random.randint(0, 500)
-    personajes.append(Npc(xaleatoria, yaleatoria))
+    radioaleatorio = random.randint(10, 30)
+    personajes.append(Npc(xaleatoria, yaleatoria,radioaleatorio))
 
 personajes_json = [p.to_dict() for p in personajes]
 
